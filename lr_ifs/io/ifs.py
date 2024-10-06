3378c1eb4c29ff98786ad80b5eef775ca36d63d2
augustinm <augustinm@met.no>
2024-10-06 18:05:13 +0200

    use 18:00UTC met file

--- lr_ifs/io/ifs.py
+++ lr_ifs/io/ifs.py
@@ -9,16 +9,18 @@ def read_od(path: Path, datetime: datetime):
     yyyymmdd = datetime.strftime('%Y%m%d')
     vars = ["amaod550", "bcaod550", "duaod550", "niaod550", "omaod550", "ssaod550", "suaod550", "aod550"]
     ifs_file = Path(path, f'{yyyymmdd}_cIFS-12UTC_o-suite_surface.nc')
+    # select first time index: 00:00:00Z
     ds = xr.open_dataset(ifs_file)[vars].isel(time=0).load()
     return ds
 
 def read_rh(path: Path, datetime: datetime):
     # the file for dd-mm-yyyy contains forecast for dd+1-mm-yyyy
-    #datetime = datetime - timedelta(days=1)
+    datetime = datetime - timedelta(days=1)
     yyyymmdd = datetime.strftime('%Y%m%d')
     vars = ["relative_humidity_pl"]
-    ifs_file = Path(path, f'ec_atmo_0_1deg_{yyyymmdd}T000000Z_pl.nc')
-    ds = xr.open_dataset(ifs_file)[vars].isel(time=0).load()
+    ifs_file = Path(path, f'ec_atmo_0_1deg_{yyyymmdd}T180000Z_pl.nc')
+    # select third time index: 00:00:00Z (the two first being 18, 21)
+    ds = xr.open_dataset(ifs_file)[vars].isel(time=2).load()
     # convert longitude from -180 180 to 0 360
     ds = ds.assign_coords(
         longitude=((ds.longitude + 360) % 360)
